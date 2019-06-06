28b6de0d08b2bb22881e2e7bee85bf9b2c524e86
TheReptilianSet <ineka3d@gmail.com>
2019-06-06 20:20:03 +0300

    Изменены цвета графиков, multi select изменен на select, Title содержит наименование, артикул, код изделия, Легенда имеет две подписи Реализации, Заказы

--- run.py
+++ run.py
@@ -4,10 +4,11 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
+from flask import Flask
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = Flask(__name__)
+app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
 # server = app.server
 app.config.suppress_callback_exceptions = True
 
@@ -32,7 +33,7 @@ orders = pd.read_excel(
 select_data = pd.concat([real[['nom', 'art', 'code']], orders[['nom', 'art', 'code']]])
 unique_codes = select_data['code'].unique()
 select_data = select_data[select_data['code'].isin(unique_codes)]
-select_data['label'] = select_data.apply(lambda x: "{}, {}".format(x['nom'], x['art']), axis=1)
+select_data['label'] = select_data.apply(lambda x: "{}, {} ({})".format(x['nom'], x['art'], x['code']), axis=1)
 select_data['value'] = select_data['code']
 
 multiselect_options = select_data[['label', 'value']].to_dict('records')
@@ -40,7 +41,7 @@ multiselect_options = select_data[['label', 'value']].to_dict('records')
 
 app.layout = html.Div([
     html.Div([html.H1("Графики заказов и реализаций")], style={'textAlign': 'center'}),
-    html.Div([dcc.Dropdown(id="selected-value",value="Ц1025719", multi=True, options=multiselect_options)], className='row', style={"display": "block", "margin-left": "auto", "margin-right": "auto"}),
+    html.Div([dcc.Dropdown(id="selected-value", value="Ц1025556", options=multiselect_options)], className='row', style={"display": "block", "margin-left": "auto", "margin-right": "auto"}),
     html.Div([dcc.Graph(id="my-graph")], style={"height": "100%"}),
     # html.Div([dcc.RangeSlider])
 ], className="container-fluid")
@@ -49,39 +50,52 @@ app.layout = html.Div([
 def create_trace(code):
     real_df = real[real['code'].isin([code])]
     order_df = orders[orders['code'].isin([code])]
-    nomenclature = "{}, {}".format(real_df['nom'].values[0], real_df['art'].values[0])
+    nomenclature = "{}, {} ({})".format(real_df['nom'].values[0], real_df['art'].values[0], real_df['code'].values[0])
 
     local_trace = [
         go.Scatter(
             x=months_ru,
             y=real_df.iloc[0, 3:],
-            name="{} (реализации)".format(nomenclature),
-            mode='lines',
-            marker={'size': 8, "opacity": 0.6, "line": {"width": 0.5}}
+            name="Реализации",
+            mode='lines+markers',
+            opacity = 0.6,
+            line = dict(
+                color=('rgb(22, 96, 167)'),
+                width=4,
+            )
         ),
         go.Scatter(
             x=months_ru,
             y=order_df.iloc[0, 3:],
-            name="{} (заказы)".format(nomenclature),
-            mode='lines',
-            marker={'size': 8, "opacity": 0.6, "line": {"width": 0.5}}
+            name="Заказы",
+            mode='lines+markers',
+            # marker={'size': 8, "opacity": 0.6, "line": {"width": 0.5}}
+            line=dict(
+                color=('rgb(205, 12, 24)'),
+                width=2,
+                dash='dash'
+            )
         )
     ]
 
-    return local_trace
+    return local_trace, nomenclature
 
 
 @app.callback(Output("my-graph", "figure"), [Input('selected-value', 'value')])
 def update_figure(selected):
     trace = []
+    nomenclature = "Сравнительный график заказов и реализаций"
     if type(selected) == list:
         for code in selected:
-            trace += create_trace(code)
+            ltrace, nomenclature = create_trace(code)
+            trace += ltrace
     else:
-        trace += create_trace(selected)
+        ltrace, nomenclature = create_trace(selected)
+        trace += ltrace
+
 
     return {"data": trace,
-            "layout": go.Layout(title="Сравнительный график заказов и реализаций", colorway=['#fdae61', '#abd9e9', '#2c7bb6'], yaxis={"title": "Количество, шт"}, xaxis={"title": "Месяцы"})}
+            "layout": go.Layout(title=nomenclature, margin=go.layout.Margin(b=50), colorway=['#fdae61', '#abd9e9', '#2c7bb6'], yaxis={"title": "Количество, шт"}, xaxis={"title": "Месяцы"})}
 
 
 if __name__ == '__main__':
