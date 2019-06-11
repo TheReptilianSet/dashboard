169f6332c1ce35f9b55de4b2c14109a9ba71bee1
TheReptilianSet <ineka3d@gmail.com>
2019-06-11 12:41:23 +0300

    Изменена инструкция чтения файлов

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -15,22 +16,19 @@ app.config.suppress_callback_exceptions = True
 months = ['january', 'february', 'march', 'april', 'may']
 months_ru = ['Январь 2019', 'Февраль 2019', 'Март 2019', 'Апрель 2019', 'Май 2019']
 
-real = pd.read_excel(
-    'files/Реализация за 01.2019-05.2019г. пономенклатурно.xlsx',
-    skiprows=10,
-    usecols=list(range(1,9)),
-    names=['nom', 'art', 'code'] + months
-).fillna(0)
+directory = 'files'
+dnames = []
+data = []
+for file in os.listdir(directory):
+    dnames.append(file.split(' ')[1])
+    data.append(pd.read_excel(
+        os.path.join(directory, file),
+        skiprows=10,
+        usecols=list(range(1,9)),
+        names=['nom', 'art', 'code'] + months
+    ).fillna(0))
 
-orders = pd.read_excel(
-    'files/Заказы покупателей за 01.2019-05.2019г. пономенклатурно.xlsx',
-    skiprows=11,
-    usecols=list(range(1,9)),
-    names=['nom', 'art', 'code'] + months
-).fillna(0)
-
-
-select_data = pd.concat([real[['nom', 'art', 'code']], orders[['nom', 'art', 'code']]])
+select_data = pd.concat(data)
 unique_codes = select_data['code'].unique()
 select_data = select_data[select_data['code'].isin(unique_codes)]
 select_data['label'] = select_data.apply(lambda x: "{}, {} ({})".format(x['nom'], x['art'], x['code']), axis=1)
@@ -38,9 +36,8 @@ select_data['value'] = select_data['code']
 
 multiselect_options = select_data[['label', 'value']].to_dict('records')
 
-
 app.layout = html.Div([
-    html.Div([html.H1("Графики заказов и реализаций")], style={'textAlign': 'center'}),
+    html.Div([html.H1("Графики заказов, реализаций, остатков")], style={'textAlign': 'center'}),
     html.Div([dcc.Dropdown(id="selected-value", value="Ц1025556", options=multiselect_options)], className='row', style={"display": "block", "margin-left": "auto", "margin-right": "auto"}),
     html.Div([dcc.Graph(id="my-graph")], style={"height": "100%"}),
     # html.Div([dcc.RangeSlider])
@@ -48,43 +45,29 @@ app.layout = html.Div([
 
 
 def create_trace(code):
-    real_df = real[real['code'].isin([code])]
-    order_df = orders[orders['code'].isin([code])]
-    nomenclature = "{}, {} ({})".format(real_df['nom'].values[0], real_df['art'].values[0], real_df['code'].values[0])
-
-    local_trace = [
-        go.Scatter(
+    local_trace = []
+    for i in range(len(dnames)):
+        df = data[i][data[i]['code'].isin([code])]
+        nomenclature = "{}, {} ({})".format(df['nom'].values[0], df['art'].values[0], df['code'].values[0])
+        local_trace.append(go.Scatter(
             x=months_ru,
-            y=real_df.iloc[0, 3:],
-            name="Реализации",
+            y=df.iloc[0, 3:],
+            name=dnames[i],
             mode='lines+markers',
-            opacity = 0.6,
-            line = dict(
-                color=('rgb(22, 96, 167)'),
-                width=4,
-            )
-        ),
-        go.Scatter(
-            x=months_ru,
-            y=order_df.iloc[0, 3:],
-            name="Заказы",
-            mode='lines+markers',
-            # marker={'size': 8, "opacity": 0.6, "line": {"width": 0.5}}
+            opacity=0.6,
             line=dict(
-                color=('rgb(205, 12, 24)'),
-                width=2,
+                # color=('rgb(205, 12, 24)'),
+                width=i,
                 dash='dash'
             )
-        )
-    ]
-
+        ))
     return local_trace, nomenclature
 
 
 @app.callback(Output("my-graph", "figure"), [Input('selected-value', 'value')])
 def update_figure(selected):
     trace = []
-    nomenclature = "Сравнительный график заказов и реализаций"
+    nomenclature = "Сравнительный график заказов, реализаций, остатков"
     if type(selected) == list:
         for code in selected:
             ltrace, nomenclature = create_trace(code)
@@ -92,7 +75,6 @@ def update_figure(selected):
     else:
         ltrace, nomenclature = create_trace(selected)
         trace += ltrace
-
 
     return {"data": trace,
             "layout": go.Layout(title=nomenclature, margin=go.layout.Margin(b=50), colorway=['#fdae61', '#abd9e9', '#2c7bb6'], yaxis={"title": "Количество, шт"}, xaxis={"title": "Месяцы"})}
